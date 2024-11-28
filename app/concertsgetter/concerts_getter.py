551439a87823c83e95f3de2649941cf4c22837e2
Mikhail <mikha2901@gmail.com>
2024-11-28 20:44:27 +0300

    добавлено использование Scraper API

--- app/concertsgetter/concerts_getter.py
+++ app/concertsgetter/concerts_getter.py
@@ -5,6 +5,7 @@ from transliterate import translit
 from app.models.common import Concert, Artist
 from datetime import datetime as dt
 import json
+import os
 
 from typing import List
 
@@ -16,24 +17,25 @@ logger = logging.getLogger(__name__)
 class ConcertsGetter:
     def __init__(self, artists: List[Artist]):
         self.artists: List[Artist] = artists
+        self.scraper_api_token = os.getenv("SCRAPER_API_TOKEN")
+        if self.scraper_api_token is None:
+            raise Exception("token not found in env SCRAPER_API_TOKEN")
 
-    @staticmethod
-    async def extract_data_from_url(url: str):
-        for i in range(5):
-            try:
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url) as response:
-                        if response.status != 200:
-                            raise Exception(f"Not OK request: url = {url}, status = {response.status}, att = {i + 1}")
-                        text = await response.text()
-                        logger.debug(f"Successful request: url = {url}")
-                        return text
-            except Exception as e:
-                logger.error(str(e))
+    async def extract_data_from_url(self, url: str):
+        try:
+            scraper_url = f"http://api.scraperapi.com?api_key={self.scraper_api_token}&url={url}"
+            async with aiohttp.ClientSession() as session:
+                async with session.get(scraper_url) as response:
+                    if response.status != 200:
+                        raise Exception(f"Not OK request: url = {url}, status = {response.status}")
+                    text = await response.text()
+                    logger.debug(f"Successful request: url = {url}")
+                    return text
+        except Exception as e:
+            logger.error(str(e))
 
-    @staticmethod
-    async def get_time_for_concert(url: str):
-        text_html = await ConcertsGetter.extract_data_from_url(url)
+    async def get_time_for_concert(self, url: str):
+        text_html = await self.extract_data_from_url(url)
 
         soup = BeautifulSoup(text_html, "html.parser")
 
@@ -41,9 +43,8 @@ class ConcertsGetter:
 
         return hours, minutes
 
-    @staticmethod
-    async def find_concert_info(url: str, artist: Artist):
-        text_html = await ConcertsGetter.extract_data_from_url(url)
+    async def find_concert_info(self, url: str, artist: Artist):
+        text_html = await self.extract_data_from_url(url)
 
         try:
             soup = BeautifulSoup(text_html, "html.parser")
@@ -65,7 +66,7 @@ class ConcertsGetter:
 
                 year, month, day = map(int, concert_data.get("startDate").split("-"))
                 ref_for_date = concert_data["url"]
-                hours, minutes = await ConcertsGetter.get_time_for_concert(ref_for_date)
+                hours, minutes = await self.get_time_for_concert(ref_for_date)
                 concert_info.datetime = dt(year, month, day, hours, minutes)
 
                 concert_info.city = all_concerts_divs[i].find("div", class_="person-schedule-place__city").text
@@ -74,7 +75,6 @@ class ConcertsGetter:
 
                 concerts.append(concert_info)
             except Exception as e:
-                logger.warning(str(e))
                 logger.warning(f"Can't get info about a {artist.name}'s concert")
         return concerts
 
@@ -96,5 +96,5 @@ class ConcertsGetter:
         urls = await asyncio.gather(*tasks)
         urls = [url for url in urls if url is not None]
 
-        tasks = [ConcertsGetter.find_concert_info(urls[i], self.artists[i]) for i in range(len(urls))]
+        tasks = [self.find_concert_info(urls[i], self.artists[i]) for i in range(len(urls))]
         return await asyncio.gather(*tasks)
