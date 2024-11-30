84b4649d638a209819bc6be66c0efd71611b118e
Philipp Nikulenko <fmnikulenko@edu.hse.ru>
2024-12-01 02:20:42 +0300

    основной метод стал возвращать list[artist] с сопутствующими изменениями, также написан примитивный тест на сравнение

--- app/artistsgetter/artists_getter.py
+++ app/artistsgetter/artists_getter.py
@@ -8,6 +8,30 @@ from json import loads
 logger = logging.getLogger(__name__)
 
 
+class Artist:
+    """
+    Класс артиста из плейлиста
+    """
+    def __init__(self, name: str, distibution: float) -> None:
+        """
+        Конструктор класса артиста: инициализирует объект по имени и процентному соотношению 
+        """
+        self.name = name
+        self.distibution = distibution
+
+    def __str__(self) -> str:
+        """
+        Строковое представление артиста
+        """
+        return f"Артист {self.name} с частотой упоминания {self.distibution}"
+    
+    def __eq__(self, other) -> bool:
+        """
+        Компанатор для артистов
+        """
+        return self.name == other.name and abs(self.distibution - other.distibution) <= 0.02
+
+
 class ArtistsGetter:
     """
     Основной класс модуля
@@ -48,27 +72,39 @@ class ArtistsGetter:
 
         print('\n'.join(text))
 
-    def get_artists_from_playlist_by_url(self, playlist_url: str) -> list | str:
+    def get_artists_from_playlist_by_url(self, playlist_url: str) -> list[Artist]:
         """
         Метод, принимающий ссылку на плейлист и позвращающий список обнаруженных исполнителей
         """
-        #  https://music.yandex.ru/users/fnikulenko/playlists/1006
         parsed_url = playlist_url.split("/")
         playlist_id = parsed_url[-1]
         user_id = parsed_url[-3]
 
-        found_artists: Set[str] = set()
+        found_artists: dict[str, int] = dict()
+        result_artists_list: list[Artist] = list()
+
+        # Padding for cases when more than one artist on a track
+        track_count_padding = 0
         
-        try:
-            response = self.client.users_playlists(kind=playlist_id, user_id=user_id)
-            for track in response.fetch_tracks():
+        try: 
+            response_tracks = self.client.users_playlists(kind=playlist_id, user_id=user_id).fetch_tracks()
+            for track in response_tracks:
+                track_count_padding += -1 + len(track.track.artists)
+
                 for artist in track.track.artists:
-                    found_artists.add(artist.name)
+                    if artist.name in found_artists:
+                        found_artists[artist.name] += 1
+                    else:
+                        found_artists[artist.name] = 1
 
-            return list(found_artists)
+            for artist_name, artist_distibution in found_artists.items():
+                result_artists_list.append(Artist(
+                        name = artist_name,
+                        distibution = round(artist_distibution / (len(response_tracks) + track_count_padding), 2)
+                    )
+                )
+
+            return sorted(result_artists_list, key=lambda x: (-x.distibution, x.name))
+        
         except NotFoundError as e:
-            return f"Трек пользователя {user_id} с идентификатором {playlist_id} не найден или не является публичным"
-
-
-
-
+            return f"Плейлист пользователя {user_id} с идентификатором {playlist_id} не найден или не является публичным"
--- app/artistsgetter/artists_getter_test.py
+++ app/artistsgetter/artists_getter_test.py
@@ -1,4 +1,34 @@
 import logging
+import os
+import unittest
+from unittest.mock import patch, MagicMock
+from artists_getter import ArtistsGetter, Artist
 
 
 logger = logging.getLogger(__name__)
+
+
+def test_get_artists_from_playlist():
+    ag = ArtistsGetter(os.getenv("YANDEX_MUSIC_TOKEN"))
+    artists = ag.get_artists_from_playlist_by_url(playlist_url="https://music.yandex.ru/users/fnikulenko/playlists/1007")
+
+    expected_return_list = [
+            Artist(name="GONE.Fludd", distibution=0.38),
+            Artist(name="MAYOT", distibution=0.25),
+            Artist(name="SHAMAN", distibution=0.12),
+            Artist(name="ЛСП", distibution=0.12),
+            Artist(name="Люся Чеботина", distibution=0.12),
+        ]
+    
+    try:
+        for i in range(len(artists)):
+            print(f"{i} real — {artists[i]}")
+            print(f"{i} expected — {expected_return_list[i]}", end="\n\n")
+            assert artists[i] == expected_return_list[i]
+    except Exception as e:
+        print(e)
+
+
+
+if __name__ == '__main__':
+    test_get_artists_from_playlist()
